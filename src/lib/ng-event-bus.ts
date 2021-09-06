90492a280125989987f61826e20085055952a61d
John Cutburth <john.cutburth@cerner.com>
2021-09-06 01:09:51 -0500

    Add data caching and behavior like BehaviorSubject including getValue

--- src/lib/ng-event-bus.ts
+++ src/lib/ng-event-bus.ts
@@ -16,6 +16,11 @@ export class NgEventBus {
    * Main observable to multicast to all observers.
    */
   private eventBus: Subject<IEventBusMessage>;
+  
+  /**
+   * Map of cached data.
+   */
+  private values: Map<string, any>;
 
   /**
    * Key message separator.
@@ -27,6 +32,7 @@ export class NgEventBus {
    */
   constructor() {
     this.eventBus = new Subject<IEventBusMessage>();
+    this.values = new Map<string, any>();
   }
 
   /**
@@ -70,15 +76,41 @@ export class NgEventBus {
   }
 
   /**
+   * function to run after observable is subscribe to
+   */
+  private after_on(key: string) {
+    this.values.forEach((data, k) => {
+      if (this.keyMatch(k, key)) {
+        const metadata: MetaData = new MetaData(k, data);
+        this.eventBus.next({ key, data, metadata });
+      }
+    });
+  }
+
+  /**
+   * Get cached data by key.
+   *
+   * @param  key Key to identify the message/event.
+   */
+  public getValue(key: string) {
+    if (this.values.has(key)) return this.values.get(key);
+  }
+
+  /**
    * Publish a message/event to event bus.
    *
    * @param  key Key to identify the message/event.
    * @param  [data] Optional: Additional data sent with the message/event.
+   * @param  boolean Optional: If true the date will be cached.
    * @throws {Error} key parameter must be a string and must not be empty.
    */
-  public cast(key: string, data?: any): void {
+  public cast(key: string, data?: any, cache: boolean = false): void {
     if (!key.trim().length) {
       throw new Error('key parameter must be a string and must not be empty');
+    }
+
+    if (cache) {
+      this.values.set(key, data);
     }
 
     const metadata: MetaData = new MetaData(key, data);
@@ -94,6 +126,8 @@ export class NgEventBus {
    * @return Observable you can subscribe to listen messages/events.
    */
   public on<T>(key: string): Observable<MetaData> {
+    setTimeout(() => this.after_on(key), 0, key);
+
     return this.eventBus.asObservable().pipe(
       filter((event: IEventBusMessage) => this.keyMatch(event.key, key)),
       map((event: IEventBusMessage) => event.metadata)
